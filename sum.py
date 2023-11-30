6225c9398e08990430dce81b5dd0f3b633a0fb06
thamiresfff <152559104+thamiresfff@users.noreply.github.com>
2023-11-30 23:55:16 +0000

    [feat]: created sum_two_numbers()

---
+++ sum.py
@@ -0,0 +1,18 @@
+def soma(
+        a:float, 
+        b:float =5
+    ) -> float:
+    """Sum two values
+    
+    Take two variabels and sum.
+
+    Args:
+        a (float): First number.
+        b (float, optional): Second number. Defaults to 5.
+
+    Returns:
+        float: _description_
+    """
+    number = a + b
+
+    return number
